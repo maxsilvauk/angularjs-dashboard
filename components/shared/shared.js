2e76f48c7e73450a9c56f49f5d8750f2b31e52d4
Max Silva <maxs@paxport.net>
2017-09-14 11:32:27 +0100

    Sliding nav

--- components/shared/shared.js
+++ components/shared/shared.js
@@ -7,17 +7,37 @@ var shared = angular.module('shared', []);
  * @param $location
  */
 shared.controller('navCtrl', function($scope, $location) {
+
+	// Controler vars.
+	var nav = '.site-nav';
+	
+	// isActive function.
 	$scope.isActive = function (viewLocation) {
-
-		// Set hamburger icon to open. 
-		//$('#hamburger-icon').addClass('open');
-
-		// Return location path.
+		// Set hamburger icon to open & return location path.
+		$('#hamburger-icon').addClass('open');
         return viewLocation === $location.path();
     };
 
-    // Toggle hamburger icon on click.
+    // Hamburger icon event listener.
     $('#hamburger-icon').click(function() {
-		$(this).toggleClass('open');
+		$(nav+'.site-nav').toggleClass('condensed');
+
+		// Open nav.
+		if (!$(nav).hasClass('condensed')) {
+			$(nav).removeClass('col-lg-1').addClass('col-lg-2');
+			$('.site-container #right').removeClass('col-lg-11').addClass('col-lg-10');
+
+			var fadeInNav = function() {
+				$(nav+' ul li span').removeClass('hide');
+			};
+
+			setTimeout(fadeInNav, 200);
+
+		// Condense nav.
+		} else {
+			$(nav+' ul li span').addClass('hide');
+			$(nav).removeClass('col-lg-2').addClass('col-lg-1');
+			$('.site-container #right').removeClass('col-lg-10').addClass('col-lg-11');
+		}
 	});
 });
