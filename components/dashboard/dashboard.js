42b66937cafa0ca54336ab3db3d732103dbcca30
Max Silva <maxs@paxport.net>
2017-09-19 12:55:45 +0100

    Google OAuth stuff

--- components/dashboard/dashboard.js
+++ components/dashboard/dashboard.js
@@ -37,6 +37,14 @@ dashboard.factory('Data', function($http, $q) {
  */
 dashboard.controller('dashboardCtrl', function($http, $q, $scope, Data) {
 
+    if (S_PersonalData.s_getPersonalData().isLogin == 'yes') {
+        $scope.name = S_PersonalData.s_getPersonalData().name;
+        $scope.email = S_PersonalData.s_getPersonalData().email;
+        $scope.profileURL = S_PersonalData.s_getPersonalData().profile;
+    } else {
+        $location.path("/");
+    }
+
     // Pull up divider
     $('.pull-up a').click(function(event) {
         event.preventDefault();
